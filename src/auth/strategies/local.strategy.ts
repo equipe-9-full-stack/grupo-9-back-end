73a1629294a984599d83275a89f6e1d29e33af4f
lianeiv <julianeiv4@gmail.com>
2026-05-22 12:48:46 -0300

    feat: adiciona import do IsPublic no UsuariosController e libera rota POST

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
-import { UnauthorizedError } from '../errors/unauthorized.error';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -16,7 +15,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, pass: string): Promise<any> {
     const user = await this.authService.validateUser(email, pass);
     if (!user) {
-      throw new UnauthorizedError('Credenciais inválidas.');
+      throw new UnauthorizedException('Credenciais inválidas.');
     }
     return user;
   }
